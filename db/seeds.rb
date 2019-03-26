c48d45cf7b593fb4b063ccdaf5cfb72938c7f787
Selcuk Karakus <selchuck.karakus@gmail.com>
2019-03-26 21:58:28 +0000

    fine tuning front-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'Cleaning Data Base'
+Ingredient.destroy_all
+puts 'Creating Ingredients...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+cocktail_list = JSON.parse(open(url).read)
+
+cocktail_list.each do |drink|
+  Ingredient.create(name: drink['strIngredient1'])
+  p drink['strIngredient1']
+end
+
+puts 'This is the end..!'
